ad437cf3af3c6e1b8bb526fbcaaacf3cc374542b
Yuthala <dinapolousova@gmail.com>
2026-01-06 21:57:26 +0300

    +lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,11 @@
 import { Manrope} from 'next/font/google';
+import { Lusitana} from 'next/font/google';
 
 export const manrope = Manrope({
   subsets: ['latin', 'cyrillic']
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', "700"]
 });
  
